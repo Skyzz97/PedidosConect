99f57a58aa4b2440950f7f4f3bbaae909e053bd8
Skyzz97 <felipelovatti2@gmail.com>
2025-06-25 15:45:26 -0300

    Create DejaVuSans.php

---
+++ tfpdf/font/unifont/DejaVuSans.php
@@ -0,0 +1,23 @@
+<?php
+// FONTE: DejaVuSans.ttf
+$type = 'TrueTypeUnicode';
+$name = 'DejaVuSans';
+$desc = array(
+  'Ascent' => 928,
+  'Descent' => -236,
+  'CapHeight' => 928,
+  'Flags' => 4,
+  'FontBBox' => '[-1021 -463 1793 1232]',
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 600
+);
+$up = -63;
+$ut = 44;
+$ttffile = __DIR__ . '/DejaVuSans.ttf'; // <- ESSE CAMINHO É FUNDAMENTAL
+$originalsize = 757076;
+$fontkey = 'dejavu';
+$enc = '';
+$diff = '';
+$file = ''; // Se for embutido por PHP, deixa vazio
+$ctg = '';
