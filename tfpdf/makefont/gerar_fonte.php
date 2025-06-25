51170583f41ecb9ce10c2e85ace6dc57684fd6cf
Skyzz97 <felipelovatti2@gmail.com>
2025-06-25 15:35:57 -0300

    Delete tfpdf/makefont directory

--- tfpdf/makefont/gerar_fonte.php
+++
@@ -1,5 +0,0 @@
-<?php
-require('makefont/makefont.php');
-
-// Gera a fonte DejaVuSans com codificação padrão
-MakeFont('DejaVuSans.ttf', 'cp1252');
--- tfpdf/makefont/makefont.php
+++
@@ -1,5 +0,0 @@
-<?php
-require('makefont/makefont.php');
-
-// Gere a fonte sem caminho absoluto
-MakeFont('DejaVuSans.ttf', 'cp1252');
