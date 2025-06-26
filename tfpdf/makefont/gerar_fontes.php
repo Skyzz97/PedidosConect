c18b03bbca0e74efea04904360b8bf4a8d4172b5
Skyzz97 <felipelovatti2@gmail.com>
2025-06-26 14:03:53 -0300

    Create gerar_fontes.php

---
+++ tfpdf/makefont/gerar_fontes.php
@@ -0,0 +1,6 @@
+<?php
+require('tfpdf/makefont/makefont.php');
+
+// Caminhos relativos ou absolutos conforme sua estrutura
+MakeFont('tfpdf/font/unifont/DejaVuSans.ttf', 'cp1252', true);
+MakeFont('tfpdf/font/unifont/DejaVuSans-Bold.ttf', 'cp1252', true);
